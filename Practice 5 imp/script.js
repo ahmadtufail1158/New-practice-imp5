ce3b7687c47fd3207c9d5514f41fd604d65a4a6b
Ahmad Tufail <164312938+ahmadtufail1158@users.noreply.github.com>
2024-06-25 11:37:11 +0500

    "done"

---
+++ Practice 5 imp/script.js
@@ -0,0 +1,88 @@
+
+// let arr = [1, 2, 3, 4, 5];
+
+// // get first 2 elements of array and the remove them so they are not added to the response array without depth.
+
+// arr =arr.slice(2);
+
+// // // // Push three new elements to the end of the array
+
+// arr.push(6, 7, 8);
+
+// // Printing the New array using for loop
+
+// for (let i = 0; i < arr. length; i++) {
+
+// console.log(arr[i]);
+
+// }
+
+
+
+
+
+//  let arr = [1, 2, 3, 4, 5];
+
+//  for (let i = 0; i < arr. length; i++) {
+
+//  if (arr[i] % 2 === 0) {
+
+// console.log(i);
+
+// }
+
+// }
+
+
+
+
+
+
+// let arr = [3, 4, 5];
+
+// push two new elements on the beginning of the array.
+
+// arr = [1, 2,...arr];
+
+// Pop (deletes last element in array) from the array
+
+// arr.pop();
+
+// index was the previous code block in this article.selects random elements from an array. but assumes that you used...
+
+// for (let i = 0; i < arr. length; i++) {
+
+// console.log(arr[i]);
+
+// }
+
+
+
+
+
+// let arr = [1, 2, 3, 5];
+
+// let previousElement = arr[0];
+
+// for (let i = 1; i < arr.length; i++) {
+//   if (arr[i] > previousElement) {
+//     console.log(arr[i]);
+//   }
+//   previousElement = arr[i];
+// }
+
+
+
+
+// let arr = [1, 2, 3, 4, 5];
+
+// // // Find the index of the element 3
+// let index = arr.indexOf(3);
+
+// // // Remove all elements starting from that index
+// arr = arr.slice(0, index);
+
+// // // Use a for loop to print the new array
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
